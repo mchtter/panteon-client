6b718a1c041ba1f69e28937f47d747ad0cf0abd5
Mücahit Alperen ERYILMAZ <mchtter@gmail.com>
2021-12-07 01:23:00 +0300

    client merged backend api in heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 export const getData = async () => {
-  let players = await axios.get("http://localhost:5000/players");
+  let players = await axios.get("https://panteon-node-server.herokuapp.com/players");
 
   return players.data;
 };
 
 export const increaseData = async (data) => {
-  await axios.post("http://localhost:5000/update/increase", data);
+  await axios.post("https://panteon-node-server.herokuapp.com/update/increase", data);
 };
 
 export const decreaseData = async (data) => {
-  await axios.post("http://localhost:5000/update/decrease", data);
+  await axios.post("https://panteon-node-server.herokuapp.com/update/decrease", data);
 };
 
 export const increaseMoney = async (data) => {
-  await axios.post("http://localhost:5000/money/increase", data);
+  await axios.post("https://panteon-node-server.herokuapp.com/money/increase", data);
 };
 
 export const decreaseMoney = async (data) => {
-  await axios.post("http://localhost:5000/money/decrease", data);
+  await axios.post("https://panteon-node-server.herokuapp.com/money/decrease", data);
 };
